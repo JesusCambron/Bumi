820c420094139b1400f63ccf0127ff2b92bca525
miguelferra <miguelferra4@gmail.com>
2020-03-28 17:17:45 -0700

    Primeras 3 pantallas

--- app/src/main/java/bumi/emptyactivity/Profile.kt
+++ app/src/main/java/bumi/emptyactivity/Profile.kt
@@ -1,18 +1,12 @@
 package bumi.emptyactivity
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class Profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_profile)
     }
-
 }
