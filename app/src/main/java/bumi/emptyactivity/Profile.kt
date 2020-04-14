e775bb3b953d38f554ab1a1d458dddd212e98525
JesusCambron <manuel.cam98@gmail.com>
2020-04-13 20:06:12 -0700

    actualizacion

--- app/src/main/java/bumi/emptyactivity/Profile.kt
+++ app/src/main/java/bumi/emptyactivity/Profile.kt
@@ -1,12 +1,23 @@
 package bumi.emptyactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        var intento: Intent = Intent(this, PlantasActivity::class.java)
+
+        val boton: ImageButton = findViewById(R.id.botonPlantas) as ImageButton
+
+        boton.setOnClickListener {
+            startActivity(intento)
+        }
     }
 }
