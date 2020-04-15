965ab3ffdcf554b19f05b310f9db2fa482fecd3b
JesusCambron <manuel.cam98@gmail.com>
2020-04-15 13:29:43 -0700

    Actualizacion

--- app/src/main/java/bumi/emptyactivity/Profile.kt
+++ app/src/main/java/bumi/emptyactivity/Profile.kt
@@ -11,18 +11,6 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-<<<<<<< HEAD
-        val button_plantas = findViewById<ImageButton>(R.id.boton_plantas)
-        button_plantas.setOnClickListener{
-            val intent = Intent(this, PlantasActivity::class.java)
-            startActivity(intent)
-        }
-
-        val button_agua = findViewById<ImageButton>(R.id.boton_agua)
-        button_agua.setOnClickListener{
-            val intent = Intent(this, AguaActivity::class.java)
-            startActivity(intent)
-=======
         var intento: Intent = Intent(this, Opciones::class.java)
 
         val botonPlantas: ImageButton = findViewById(R.id.botonPlantas) as ImageButton
@@ -42,7 +30,6 @@ class Profile : AppCompatActivity() {
         botonReciclaje.setOnClickListener {
             intento.putExtra("type","reciclaje")
             startActivity(intento)
->>>>>>> jcambron
         }
     }
 }
