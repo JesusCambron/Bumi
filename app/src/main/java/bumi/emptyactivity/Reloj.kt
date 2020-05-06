7841cea35af391a13e56791e12c494858f4961bc
JesusCambron <manuel.cam98@gmail.com>
2020-05-06 16:00:11 -0700

    Reloj

--- app/src/main/java/bumi/emptyactivity/Reloj.kt
+++ app/src/main/java/bumi/emptyactivity/Reloj.kt
@@ -2,78 +2,33 @@ package bumi.emptyactivity
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.SystemClock
 import android.widget.Button
+import android.widget.Chronometer
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_reloj.*
 
 class Reloj : AppCompatActivity() {
-    var milisegundos: Int = 0;
-    var segundo: Int = 0;
-    var minuto: Int = 0;
-    var hora: Int = 0;
-    var segundoString: String = "0"
-    var minutosString: String = "0"
-    var horaString: String = "0"
-    var isOn: Boolean = false
 
-
-
+    var encendido:Boolean = false
+    var time:Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reloj)
+
         var botonTiempo: Button = findViewById(R.id.botonReloj) as Button
-        Thread(Runnable {
-            while (true) {
-                if(isOn) {
-                    if(milisegundos == 999) {
-                        milisegundos=0
-                        this.runOnUiThread(Runnable { actualizarReloj() })
-                    }
-                    Thread.sleep(1)
-                    milisegundos++
+        var cronometro: Chronometer = findViewById(R.id.cronometro)
 
-                }
-            }
-        }).start()
         botonTiempo.setOnClickListener {
-            isOn=!isOn
-            if(isOn) {
-                botonTiempo.setText("Detener")
+            encendido = !encendido
+            if(encendido) {
+                cronometro.base = SystemClock.elapsedRealtime() - time
+                cronometro.start()
             } else {
-                botonTiempo.setText("Iniciar")
+                time = SystemClock.elapsedRealtime() - cronometro.base
+                cronometro.stop()
             }
-
         }
-    }
-
-    fun actualizarReloj() {
-            segundo ++
-            if (segundo == 59) {
-                segundo = 0
-                minuto ++
-                if (minuto == 59) {
-                    minuto = 0
-                    hora ++
-                }
-            }
-            if (segundo < 10) {
-                segundoString = "0" + segundo
-            } else {
-                segundoString = "" + segundo
-            }
-
-            if (minuto < 10) {
-                minutosString = "0" + minuto
-            } else {
-                minutosString = "" + minuto
-            }
-
-            if (hora < 10) {
-                horaString = "0" + hora
-            } else {
-                horaString = "" + hora
-            }
-        vistaTiempo.setText(horaString + ":" + minutosString + ":" + segundoString)
     }
 }
