76f6782956c3c58965087c6c402ef0f55fb06676
rey <rey.echeverria7@gmail.com>
2020-05-07 01:25:07 -0700

    Pantalla Ahorro Agua

---
+++ app/src/main/java/bumi/emptyactivity/AhorroAgua.kt
@@ -0,0 +1,12 @@
+package bumi.emptyactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AhorroAgua : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ahorro_agua)
+    }
+}
