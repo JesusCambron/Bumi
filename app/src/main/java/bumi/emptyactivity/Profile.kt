b20d681d4d8cb0e07acc9c70be9e3a26ac54845a
JesusCambron <manuel.cam98@gmail.com>
2020-05-12 17:16:57 -0700

    Menu

--- app/src/main/java/bumi/emptyactivity/Profile.kt
+++ app/src/main/java/bumi/emptyactivity/Profile.kt
@@ -3,9 +3,11 @@ package bumi.emptyactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_profile.*
-
 class Profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +22,12 @@ class Profile : AppCompatActivity() {
         val botonPlantas: ImageButton = findViewById(R.id.botonPlantas) as ImageButton
         val botonAgua: ImageButton = findViewById(R.id.botonAgua) as ImageButton
         val botonReciclaje: ImageButton = findViewById(R.id.botonReciclaje) as ImageButton
-
+/*
         boton_cerrarsesion.setOnClickListener {
             finish()
         }
+
+ */
 
         botonPlantas.setOnClickListener {
             intento.putExtra("type","plantas")
@@ -39,5 +43,19 @@ class Profile : AppCompatActivity() {
             intento.putExtra("type","reciclaje")
             startActivity(intento)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        var inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.cerrarSesion -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
